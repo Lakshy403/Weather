479cf84ed3fe0c2640a9f099fe96117b6994a735
Lakshy403 <Lakshyp09@gmail.com>
2026-05-02 16:47:13 +0530

    Initial Commit

---
+++ WEATHER APP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WEATHER APP
+//
+//  Created by user@51 on 02/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WEATHER APP/WEATHER_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEATHER_APPApp.swift
+//  WEATHER APP
+//
+//  Created by user@51 on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WEATHER_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
